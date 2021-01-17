8f35269e5cd09a04adb0b611ff54c2b046850e87
YelloSpag <69869469+YelloSpag@users.noreply.github.com>
2021-01-17 13:46:07 -0800

    Added proportional turn radius and speed, Added main .ino script

--- keyboardCommands.py
+++ keyboardCommands.py
@@ -1,16 +1,25 @@
 import serial, pygame, time
 pygame.init()
 
-port = "COM3"
-arduino = serial.Serial(port, 9600)
+# port = "COM3"
+# arduino = serial.Serial(port, 9600)
+
 lx = 0.0
 ly = 0.0
 rx = 0.0
 ry = 0.0
 rt = 0.0
 lt = 0.0
-turning_sensitivity = 1.0
-turning_deadzone = 0.1
+
+turning_sensitivity = 2.0 # 0-1 float
+turning_deadzone = 0.1 # 0-1 float
+turning_radius = 0.2 # 0-1 float
+max_speed = 240 # 0-255 integer
+
+turning_sensitivity = min(max(turning_sensitivity, 0.0), 1.0)
+turning_radius = min(max(turning_radius, 0.0), 1.0)
+turning_deadzone = min(max(turning_deadzone, 0.0), 1.0)
+
 main_j = pygame.joystick.Joystick(0)
 main_j.init()
 
@@ -21,15 +30,22 @@ def create_game():
 create_game()
 
 def process_speed(trigger_vector):
-    return int(trigger_vector * 255)
+    return int(trigger_vector * max_speed)
 
 def process_turning(turning_vector, speed):
+    turning_limit = float(speed * turning_radius)
     right = speed
     left = speed
     if turning_vector > (0.5 + turning_deadzone):
+        left -= turning_limit
         left *= (1 - (turning_vector - 0.5) * 2) * turning_sensitivity
+        left += turning_limit
+        # left = max(left, turning_radius)
     elif turning_vector < (0.5 - turning_deadzone):
+        right -= turning_limit
         right *= (1 - (turning_vector - 0.5) * -2) * turning_sensitivity
+        right += turning_limit
+        # right = max(right, turning_radius)
     return [int(left), int(right)]
 
 run = True
@@ -45,7 +61,7 @@ while run:
     # print("Left x: " + lx, "Left y: " + ly, "Right x: " + rx, "Right y: " + ry, "Right trigger: " + rt, "Left trigger: " + lt)
     data = f"{str(motors[1])}|{str(motors[0])}"
     print(data)
-    arduino.write(data.encode())
+    # arduino.write(data.encode())
     # mx, my = pygame.mouse.get_pos()
     # mouse_pos = [int(mx  / (500 / 180)), int(my / (500 / 180))]
     # arduino.write(("X" + str(mouse_pos[0]) + "Y" + str(mouse_pos[1])).encode())
